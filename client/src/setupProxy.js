247d45c2d709531c39ebeccaabc49626fe79b123
Onaneye Ajibola <ajibolaonaneye@gmail.com>
2021-01-04 16:56:32 +0100

    6th commit

--- client/src/setupProxy.js
+++ client/src/setupProxy.js
@@ -1,11 +1,10 @@
-const baseUrl = "http://youtube--clone.herokuapp.com"
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
 module.exports = function (app) {
     app.use(
         '/api',
         createProxyMiddleware({
-            target: `${baseUrl}` && 'http://localhost:5000',
+            target: 'http://localhost:5000' ,
             changeOrigin: true,
         })
     );
